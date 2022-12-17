1e06b2afc2dbe536fcb2ba29b6dc0f6c65f46586
Miguel Ortega <miguelow@Miguels-MacBook-Air.local>
2022-12-17 18:54:39 +0100

    testing mobolie header bux fix

--- src/components/home/Socials.jsx
+++ src/components/home/Socials.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion'
 
 function Socials() {
   return (
-    <motion.div className="home__social" initial={{opacity: 0, x: -100}} animate={{opacity: 1, x: 50}} transition={{ease: "easeInOut", duration:1,delay:0.4}}>
+    <div className="home__social">
       <a
         rel="noreferrer"
         href="https://www.linkedin.com/in/miguel-ortega-ward/"
@@ -21,7 +21,7 @@ function Socials() {
       >
         <i class="uil uil-github"></i>
       </a>
-    </motion.div>
+    </div>
   );
 }
 
