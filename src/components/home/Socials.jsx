9b940f86d9d313280facf6f80acd8cafdd397905
Miguel Ortega <miguelow@Miguels-MacBook-Air.local>
2022-12-17 18:50:46 +0100

    fixed contact block

--- src/components/home/Socials.jsx
+++ src/components/home/Socials.jsx
@@ -1,9 +1,10 @@
 
 import React from 'react'
+import { motion } from 'framer-motion'
 
 function Socials() {
   return (
-    <div className="home__social">
+    <motion.div className="home__social" initial={{opacity: 0, x: -100}} animate={{opacity: 1, x: 50}} transition={{ease: "easeInOut", duration:1,delay:0.4}}>
       <a
         rel="noreferrer"
         href="https://www.linkedin.com/in/miguel-ortega-ward/"
@@ -20,7 +21,7 @@ function Socials() {
       >
         <i class="uil uil-github"></i>
       </a>
-    </div>
+    </motion.div>
   );
 }
 
