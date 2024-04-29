ad923b92bace0036ce0d6ff995e3a0839811a5ea
Miguel Ortega Ward <miguel.owd@gmail.com>
2024-04-29 16:30:52 +0200

    updated cv and cleaned up the code

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./About.css";
 import AboutImg from "../../assets/profile.jpeg";
-import CV from "../../assets/MOW-english-CV.pdf";
+import CV from "../../assets/CVESP.pdf";
 import Info from "./Info.jsx"
 import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 
